dd79c656242c649752d0c8351ed34b7dce0c5dba
Alejandro Sorza <Jasorza5@misena.edu.co>
2019-05-13 17:06:03 -0500

    1

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Linq
 {
@@ -6,7 +7,63 @@ namespace Linq
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // 
+            // 
+            //Obtener el promedio de la duracion de todos los proyectos 
+            //Obtener el nombre del proyecto con mas duracion 
+            //Selecionar el nombre y area del proyecto con duracion mayor a 6 
+            //Verificar si hay un proyecto con una duracion superior a 24 
+            //Sumar las duraciones de los proyectos cuyo codigo sea superior a 20 
+            //Tomar los dos primeros proyectos cuyo codigo sea superior a 30 
+
+            Console.WriteLine("** EJERCICIO **");
+            Proyecto project = new Proyecto();
+            project.proyectos = project.AgregarProyecto2();
+            var proy = project.proyectos;
+            Console.ReadLine();
+
+
+            Console.WriteLine("-----------Obtener todos los proyectos cuya duración sea superior a 6 y inferior a 12-----------");
+            var supinf = proy.Where(x => x.Duracion > 6 && x.Duracion < 12).ToList();
+            project.Imprimir(supinf);
+
+            Console.WriteLine("-----------Obtener todos los proyectos que empiecen por (B) -----------");
+            var proyectosb = from p in proy
+                             where (p.Nombre.StartsWith("B"))
+                             select p;
+            project.Imprimir(proyectosb.ToList());
+            
+
+            Console.WriteLine("-----------Obtener todos los proyectos cuyo codigo sea multiplo de 7-----------");
+            var proyectosmult = proy.Where(x => x.Codigo % 7 == 0).ToList();
+            project.Imprimir(proyectosmult);
+
+            Console.WriteLine("-----------Agrupar los proyectos que tenga una duracion superior a 6 y inferior a 10 y los que sean mayor a 10-----------");
+            var gruposProys = proy.GroupBy(x =>
+            {
+                if (x.Duracion < 10)
+                {
+                    return "Son menores que 10";
+                }
+                else if (x.Duracion > 6)
+                {
+                    return "Son mayores de 6";
+                }
+                else
+                {
+                    return "son mayores de 10";
+                }
+            });
+            foreach (var grupoProy in gruposProys)
+            {
+                Console.WriteLine($"Grupo de proyectos {grupoProy.Key}-----Cantidad-----{grupoProy.Count()}");
+                foreach (var item in grupoProy)
+                {
+                    Console.WriteLine($"El nombre del proyecto {item.Nombre} \r\n Codigo Proyecto {item.Codigo}");
+                }
+            }
+            Console.WriteLine();
+            Console.ReadLine();
         }
     }
 }
---
+++ Proyecto.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Linq
+{
+    class Proyecto
+    {
+        public List<Proyecto> proyectos = new List<Proyecto>();
+
+        public string Nombre { get; set; }
+        public double Codigo { get; set; }
+        public string Area { get; set; }
+        public int Duracion { get; set; }
+
+        //public List<Proyecto> AgregarProyecto()
+        //{
+        //    Console.WriteLine("Escriba (Y) para ingresar");
+        //    var res = Console.ReadLine();
+        //    while (res == "Y")
+        //    {
+        //        Console.WriteLine("Escriba el nombre del proyecto");
+        //        var nom = Console.ReadLine();
+        //        Console.WriteLine("Escriba el codigo del proyecto");
+        //        var cod = Console.ReadLine();
+        //        var miProyecto = new Proyecto()
+        //        {
+
+        //        };
+        //        proyectos.Add(miProyecto);
+        //        Console.WriteLine("Escriba (Y) para ingresar");
+        //        res = Console.ReadLine();
+        //    }
+        //    return proyectos;
+        //}
+
+        public List<Proyecto> AgregarProyecto2()
+        {
+            var proys = new List<Proyecto>()
+            {
+                new Proyecto()
+                {
+                    Nombre = "Bancaria",
+                    Codigo = 23,
+                    Area = "Comercio",
+                    Duracion = 9
+                },
+
+                new Proyecto()
+                {
+                    Nombre = "Mercadeo",
+                    Codigo = 23,
+                    Area = "Talento Humano",
+                    Duracion = 6
+                },
+
+                new Proyecto()
+                {
+                    Nombre = "Atención Primaria",
+                    Codigo = 23,
+                    Area = "AIPI",
+                    Duracion = 8
+                },
+
+                new Proyecto()
+                {
+                    Nombre = "Analisis y Desarrollo de Sistemas de Información",
+                    Codigo = 27,
+                    Area = "Sistemas",
+                    Duracion = 18
+                },
+
+                new Proyecto()
+                {
+                    Nombre = "Producción Multimedia",
+                    Codigo = 7,
+                    Area = "Sistemas",
+                    Duracion = 18
+                },
+
+                new Proyecto()
+                {
+                    Nombre = "Producción Multimedia",
+                    Codigo = 27,
+                    Area = "Sistemas",
+                    Duracion = 18
+                }
+
+            };
+            return proys;
+        }
+
+        public void Imprimir(List<Proyecto> proy)
+        {
+            Console.WriteLine("-----------Lista de Proyectos-----------");
+ 
+            foreach (var item in proy)
+            {
+                Console.WriteLine($"    Nombre {item.Nombre}");
+                Console.WriteLine($"    Codigo {item.Codigo}");
+                Console.WriteLine($"    Area {item.Area}");
+                Console.WriteLine($"    Duracion {item.Duracion}");
+                Console.WriteLine("---------------------------------------");
+
+            }
+        }
+    }
+}
